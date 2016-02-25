fcc28af72ace9194d912f9c097e65e281670e6a5
Jeffery Schons <jcschons@iastate.edu>
2016-02-24 22:15:49 -0600

    started coding

---
+++ dungiongame2/src/map/map.java
@@ -0,0 +1,24 @@
+package map;
+
+public class map {
+	public void initalize()
+	{
+		
+	}
+	
+	public room getRoom()
+	{
+		
+	}
+	
+	public char[] returnMap()
+	{
+		
+	}
+	
+}
+
+class room
+{
+	
+}
